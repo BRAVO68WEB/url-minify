028dcad27d2481d1ea2996c11e22cb65ab6f3508
BRAVO68WEB <jbandyopadhayaya@gmail.com>
2022-02-01 03:04:11 +0530

    Fixs #17

--- backend/controllers/minify.js
+++ backend/controllers/minify.js
@@ -1,13 +1,14 @@
 const Minfy = require("../models/minifed_urls")
+const base_url = 'https://minfy.xyz/'
 
-module.exports.getURLData = async (req,res) => {
-    try {
-        const {alias} = req.params;
-        const data = await Minfy.findOne({minifiedUrl: alias});
-
-        return res.send( data);
-    }catch(err){
-        console.error(err)
-        res.sendStatus(500)
-    }
+module.exports.getURLData = async (req, res) => {
+   try {
+      const { alias } = req.params
+      const data = await Minfy.findOne({ alias: alias })
+      data.minifiedUrl = base_url + data.alias
+      return res.send(data)
+   } catch (err) {
+      console.error(err)
+      res.sendStatus(500)
+   }
 }
