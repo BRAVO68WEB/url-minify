a995f2052e24d0a6d6daa7403a3cf3577a9c2894
Pratyay Roy <59203815+DarkFalc0n@users.noreply.github.com>
2022-02-08 20:51:06 +0530

    Sign Up created

---
+++ frontend/components/Reg/Reg.style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export default styled.section`
+  display: flex;
+  justify-content:center;
+  align-items:center;
+  .content {
+    display: flex;
+    justify-content:center;
+    align-items:center;
+    flex-direction:column;
+  }
+`
