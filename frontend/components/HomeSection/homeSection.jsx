1067b4bcafb35a2d33eabebfd60069df60ba937f
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2022-02-13 11:32:34 +0530

    added axios handlers

--- frontend/components/HomeSection/homeSection.jsx
+++ frontend/components/HomeSection/homeSection.jsx
@@ -1,82 +1,80 @@
-import React from 'react';
+import React from "react";
 import HomeSectionStyle from "./HomeSection.style";
-import Link from 'next/link';
+import Link from "next/link";
+import Axios from "helpers/Axios";
 
 const head = {
-    fontSize: '5rem',
-    fontWeight: 'bold',
-    marginBottom: '30px'
+	fontSize: "5rem",
+	fontWeight: "bold",
+	marginBottom: "30px",
 };
 const box = {
-    fontSize: '1.5em',
-    borderRadius: '50px',
-    padding: '1em',
-    width: '500px',
-    height: '50px'
+	fontSize: "1.5em",
+	borderRadius: "50px",
+	padding: "1em",
+	width: "500px",
+	height: "50px",
 };
 const btn = {
-    fontWeight: 'bold',
-    position: 'absolute',
-    alignItems: 'center',
-    right: '0px',
-    marginRight: '5px',
-    marginTop: '4.5px',
-    marginBottom: '3px',
-    height: '40px',
-    width: '100px',
-    borderRadius: '50px'
-
+	fontWeight: "bold",
+	position: "absolute",
+	alignItems: "center",
+	right: "0px",
+	marginRight: "5px",
+	marginTop: "4.5px",
+	marginBottom: "3px",
+	height: "40px",
+	width: "100px",
+	borderRadius: "50px",
 };
 const searchBox = {
-    position: 'relative',
-
+	position: "relative",
 };
 function HomeSection(props) {
+	const setMinfy = async () => {
+		let res;
+		try {
+			res = await Axios.post(`/minify/add`, {
+				originalUrl: props.longUrl,
+			});
+		} catch (err) {
+			console.error(err);
+			return;
+		}
 
-    const setMinfy = async () => {
-        var API_URL = 'http://localhost:5000';
-        const res = await fetch(`${API_URL}/minify/add`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
+		const data = await res.data;
+		props.setShortUrl(data.minifiedUrl);
+		navigator.clipboard.writeText(props.shortUrl);
+	};
 
-            },
-            body: JSON.stringify({
-                originalUrl: props.longUrl
-            }),
-        });
+	return (
+		<HomeSectionStyle>
+			<div className="content">
+				<h1 style={head} className="title">
+					URL MINIFY
+				</h1>
 
-        if (res===null) {
-            console.log("error"); 
-            return;
-        }
-
-        const data = await res.json();
-        props.setShortUrl(data.minifiedUrl);
-        navigator.clipboard.writeText(props.shortUrl);
-    }
-
-
-
-    return (
-        <HomeSectionStyle>
-            <div className="content">
-                <h1 style={head} className="title">
-                    URL MINIFY
-                </h1>
-
-                <div style={searchBox}>
-                    <input style={box} placeholder="Enter the url to be minified......" value={props.longUrl} onChange={(e) => { props.setLongUrl(e.target.value) }} />
-                    <button style={btn} id="minify" onClick={setMinfy}>MINIFY</button>
-                </div>
-                <div>
-                    <h3>Need more advanced features? | <Link href="/signup">Create an account</Link></h3>
-                </div>
-            </div>
-        </HomeSectionStyle>
-    );
+				<div style={searchBox}>
+					<input
+						style={box}
+						placeholder="Enter the url to be minified......"
+						value={props.longUrl}
+						onChange={(e) => {
+							props.setLongUrl(e.target.value);
+						}}
+					/>
+					<button style={btn} id="minify" onClick={setMinfy}>
+						MINIFY
+					</button>
+				</div>
+				<div>
+					<h3>
+						Need more advanced features? | <Link href="/signup">Create an account</Link>
+					</h3>
+				</div>
+			</div>
+		</HomeSectionStyle>
+	);
 }
-
 
 export default HomeSection;
