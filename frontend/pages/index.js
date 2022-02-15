b542cf1f7cba39800016470a8bcde06e398c3d3b
Pratyay Roy <59203815+DarkFalc0n@users.noreply.github.com>
2022-02-15 20:19:59 +0530

    added login and reg page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,25 +1,29 @@
-import { useState } from 'react'
-import Head from 'next/head'
-import HomeSection from "components/HomeSection/homeSection"
+import { useState, useMemo } from "react";
+import Head from "next/head";
+import HomeSection from "components/HomeSection/homeSection";
 import NavBar from "components/NavBar";
-import Features from 'components/Features'
-
+import Features from "components/Features";
+import Reg from "components/Reg/Reg";
+import Login from "components/Login/Login";
+import { UserAuthProvider, UserContext } from "./user/usercontext";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export default function Home() {
-   const [shortUrl, setShortUrl] = useState(null)
-   const [longURL, setLongURL] = useState('')
-   return (
-      <div className={""}>
-         <Head>
-            <title>URL MiniFy</title>
-            <link rel="icon" href="/favicon.ico" />
-         </Head>
+  const [shortUrl, setShortUrl] = useState(null);
+  const [longURL, setLongURL] = useState("");
 
-         <main className={"main-bg"}>
-             <NavBar />
-             <HomeSection />
-             <Features/>
-         </main>
-      </div>
-   )
+  return (
+    <>
+      <Head>
+        <title>URL MiniFy</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={"main-bg"}>
+        <NavBar />
+        <HomeSection />
+        <Features />
+      </main>
+    </>
+  );
 }
