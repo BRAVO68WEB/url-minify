b07d410016ef62973cf1ed41e3d927a4b9c1aae1
chirag <chiragbhalotia0412@gmail.com>
2022-02-19 20:28:28 +0530

    started with dashboard

--- frontend/components/Dashboard/Dashboard.jsx
+++ frontend/components/Dashboard/Dashboard.jsx
@@ -1,25 +1,45 @@
 import DashboardStyle from "./Dashboard.style";
 
-function Sidebar1(){
-    return(
-        <div className="sidebar" style={{backgroundColor:"#696969"}}>
+function Sidebar1() {
+    return (
+        <div className="sidebar">
             <ul>
-                <li className="nav-item mb-2"><h3>Brand Name</h3></li>
-                <li className="nav-item mb-2"><img src="logo.svg" style={{'borderRadius':'5px'}}></img></li>
-                <li className="nav-item mb-2">Hyat</li>
-                <li className="nav-item mb-2">Product Desiner</li>
-                <li className="nav-item mb-2"><button>Create Link</button></li>
-                <li className="nav-item mb-2">Overview</li>
-                <li className="nav-item mb-2">MyLinks</li>
-                <li className="nav-item mb-2">Extras</li>
+                <div className="nav-item mb-2 brand-name"><h1>Brand Name</h1></div>
+                <div className="profile">
+                    <div className="profile-image"><img
+                        src="https://png.pngtree.com/png-vector/20190307/ourlarge/pngtree-vector-edit-profile-icon-png-image_760869.jpg"/>
+                    </div>
+                    <p className="name">Hayat</p>
+                    <p className="profession">Product Designer</p>
+                </div>
+                <div className="creatButton">
+                    <button className={""}>Create Link</button>
+                </div>
+                <li className="nav-buttons">
+                    <div className="nav-item">
+                        <button>
+                            Overview
+                        </button>
+                    </div>
+                    <div className="nav-item">
+                        <button>
+                            My Links
+                        </button>
+                    </div>
+                    <div className="nav-item">
+                        <button>
+                            Extras
+                        </button>
+                    </div>
+                </li>
             </ul>
         </div>
     )
 }
-function Sidebar2()
-{
-    return(
-        <div  className="sidebar2">
+
+function Sidebar2() {
+    return (
+        <div className="sidebar2">
             <h2 className="dash">Overview</h2>
             <div className="row">
                 <div className="column">Box1</div>
@@ -29,14 +49,14 @@ function Sidebar2()
         </div>
     )
 }
-function Graph()
-{
+
+function Graph() {
     return (
-        <div  className="sidebar2">
+        <div className="sidebar2">
             <div className="row">
                 <div className="column">Daily Insight</div>
                 <div className="column">
-                    <ul >
+                    <ul>
                         <li className="list2">Api Version</li>
                         <li className="list2">Frontend Version</li>
                         <li className="list2">Links generated yet</li>
@@ -52,9 +72,9 @@ function Graph()
 function App() {
     return (
         <DashboardStyle>
-            <Sidebar1 />
-            <Sidebar2 />
-            <Graph />
+            <Sidebar1/>
+            <Sidebar2/>
+            <Graph/>
         </DashboardStyle>
     );
 }
