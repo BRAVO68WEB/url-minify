602a4004f74eadbe47492dd2737b454c47c423b0
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2022-02-26 13:10:40 +0530

    started with dashboard (#70)

--- frontend/components/Dashboard/Dashboard.style.js
+++ frontend/components/Dashboard/Dashboard.style.js
@@ -2,13 +2,80 @@ import styled from "styled-components"
 
 export default styled.div`
   .sidebar{
+    background-color:#00253A;
     float: left;
     width: 20%;
     height: 100vh;
+    color:white !important;
+    .brand-name{
+      margin-top:40px;
+      padding-left:20px;
+    }
+    .profile{
+      margin:25px 0;
+      display:flex;
+      flex-direction:column;
+      justify-content: center;
+      align-items:center;
+      .profile-image{
+        max-width:35%;
+        margin:auto;
+        border-radius:100%;
+        img{
+          max-width:100%;
+          border-radius:100%;
+        }
+      }
+      .name{
+        font-size:1.2rem;
+        font-weight:bold;
+        margin: 3px 0;
+      }
+      .profession{
+        color:gray;
+        font-size:0.8rem;
+        font-weight:bold;
+        margin:  0;
+      }
+    }
+    .creatButton{
+      
+      margin:40px auto 80px;
+      width:70%;
+      button{
+        width:100%;
+        color:white;
+        font-weight:bold;
+        font-size:1rem;
+        padding:10px;
+        background-color:blueviolet;
+        outline:none;
+        border:none;
+        border-radius:5px;
+      }
+    }
   }
-  .nav-item {
-    margin: 2.5rem;
-    list-style-type: none;
+  .nav-buttons{
+    .nav-item {
+      margin:0;
+      button{
+        width:100%;
+        background-color:transparent;
+        border:none;
+        outline:none;
+        padding:10px;
+        color:white;
+        font-size:1.1rem;
+        margin:0;
+        padding-left:20px;
+        text-align: left;
+        :hover{
+          background-color:#07344A;
+          color:#43BFD6;
+          border-left:5px solid #43BFD6;
+        }
+      }
+    }
   }
   .sidebar2{
     text-align: center;
