7635770d4526bdd40ebef14d76cf315201597dd0
Tejas <tejasftw117@gmail.com>
2022-03-03 22:29:57 +0530

    fixed bugs regarding css

--- frontend/components/NavBar/Navbar.style.js
+++ frontend/components/NavBar/Navbar.style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { AppBar } from "@mui/material";
 
 export default styled(AppBar)`
-  position: fixed;
+  position: fixed !important;
   top:0;
   left:0;
   z-index:100;
