a759b411c5ffe439afb5de181ebd2247ef720f3f
akanksha <akanksha.t05@gmail.com>
2022-03-29 15:25:13 +0530

    modified footer

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -2,12 +2,14 @@ import React from 'react'
 import NavBar from 'components/NavBar'
 import Features from 'components/Features'
 import Login from 'components/Login/Login'
+import Footer from 'components/Footer/Footer'
 
 export default function signup() {
   return (
     <div className="flex-column">
       <NavBar />
       <Login />
+      <Footer />
     </div>
   )
 }
--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -3,12 +3,14 @@ import Head from 'next/head'
 import NavBar from 'components/NavBar'
 import Features from 'components/Features'
 import Reg from 'components/Reg/Reg'
+import Footer from 'components/Footer/Footer'
 
 export default function signup() {
   return (
     <div className="flex-column">
       <NavBar />
       <Reg />
+      <Footer />
     </div>
   )
 }
