157e5b6e44ca3630132f74be007f0d5e0ed14df2
akanksha <akanksha.t05@gmail.com>
2022-04-29 20:02:02 +0530

    added dark mode

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import NavBar from 'components/NavBar'
 import Features from 'components/Features'
 import Login from 'components/Login/Login'
 import Footer from 'components/Footer/Footer'
+import UserAuth, { UserContext } from '../helpers/user/usercontext'
 
 export default function signup() {
+  const { mode } = useContext(UserAuth)
   return (
-    <div className="flex-column">
+    <div className={mode == 'light' ? 'flex-column' : 'dark-bg'}>
       <NavBar />
       <Login />
       <Footer />
